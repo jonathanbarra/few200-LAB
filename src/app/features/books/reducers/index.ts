5dad6becccec4fbdcf91273d1d1966ba9cde80e1
Jeffry Gonzalez <jeff@hypertheory.com>
2019-10-08 18:50:21 +0000

    getting the books going

---
+++ src/app/features/books/reducers/index.ts
@@ -0,0 +1,10 @@
+import { ActionReducerMap } from '@ngrx/store';
+export const featureName = 'booksFeature';
+
+export interface BooksState {
+
+}
+
+export const reducers: ActionReducerMap<BooksState> = {
+
+};
